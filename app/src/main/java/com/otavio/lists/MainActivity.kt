558f18da55d2ea364761257e97dd068d623e0e32
Otavio Dutra <dutra.otavio@gmail.com>
2021-10-20 11:24:29 -0300

    Testando LazyList utilizando Jetpack Compose

---
+++ app/src/main/java/com/otavio/lists/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.otavio.lists
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            LazyColumn{
+                items(5000){
+                    Text("Item $it",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
+                }
+            }
+        }
+    }
+}
